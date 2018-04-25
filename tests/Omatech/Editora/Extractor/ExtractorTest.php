cb2104c3af29a0656769d9b1e936c0402e825d75
root <aroca@omatech.com>
2018-04-25 14:11:50 +0200

    Initial test architecture

--- tests/Omatech/Editora/Extractor/ExtractorTest.php
+++ tests/Omatech/Editora/Extractor/ExtractorTest.php
@@ -1,97 +1,72 @@
 <?php
 
-//declare(strict_types = 1);
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-//echo __DIR__."\n";die;
+require_once __DIR__ . '/../TestCaseBase.php';
 
-
-$autoload_location = '/vendor/autoload.php';
-while (!is_file(__DIR__.$autoload_location)) { $autoload_location='/..'.$autoload_location;}
-
-$config_location = '/conf/config.php';
-while (!is_file(__DIR__.$config_location)) { $config_location='/..'.$config_location;}
-
-$bootstrap_location = '/conf/bootstrap.php';
-while (!is_file(__DIR__.$bootstrap_location)) { $bootstrap_location='/..'.$bootstrap_location;}
-
-require_once __DIR__.$autoload_location;
-require_once __DIR__.$config_location;
-require_once __DIR__.$bootstrap_location;
-
-use PHPUnit\Framework\TestCase;
 use Omatech\Editora\Extractor\Extractor;
 
-final class ExtractorTest extends PHPUnit_Framework_TestCase {
+final class ExtractorTest extends TestCaseBase
+{
 
-    public function testHello() 
-		{
+    public function testHello()
+    {
         $this->assertEquals('hello', 'hello');
     }
-		
-		public function testProblemaID(){
-      global $conn;
-			$id=-1;
+
+    public function testProblemaID()
+    {
+        $id = -1;
         $params = [
-          'lang' => 'ca'
-          , 'debug' => true
-          , 'metadata' => true
-					, 'show_inmediate_debug' => true
-					, 'timings' => true
-        ];			
-				
-				$e=new Extractor($conn, $params);
-				$res=$e->findInstanceById($id, null, function($i) use ($e)
-				{//page
-					$submenus=$e->findRelatedInstances($i, 'obc_section_pages', 1, ['alias'=>'submenu'], function($i) use ($e){
-						return $e->findRelatedInstances($i, 'obc_section_pages', 100, ['direction'=>'child', 'alias'=>'pages']);
-					});
-					//print_r($submenus);die;
-					$groups=$e->findRelatedInstances($i, 'page_groupsmusicians', 100, ['alias'=>'groups'], function($i) use ($e)
-						{
-						  return $e->findChildrenInstances($i, 'group_musicians', 100, ['alias'=>'musicians']);
-						});
-				  return array_merge($submenus, $groups);	
-				});
-				
-				$this->assertEquals(array(), $res);
-				
-		}				
+            'lang' => 'ca'
+            , 'debug' => true
+            , 'metadata' => true
+            , 'show_inmediate_debug' => true
+            , 'timings' => true
+        ];
 
-		public function testOBC2(){
-      global $conn;
-			
-			$start= microtime(true);
-			
-			$id=25116;
+        $e = new Extractor($this->connection, $params);
+        $res = $e->findInstanceById($id, null, function ($i) use ($e) {//page
+            $submenus = $e->findRelatedInstances($i, 'obc_section_pages', 1, ['alias' => 'submenu'], function ($i) use ($e) {
+                return $e->findRelatedInstances($i, 'obc_section_pages', 100, ['direction' => 'child', 'alias' => 'pages']);
+            });
+            //print_r($submenus);die;
+            $groups = $e->findRelatedInstances($i, 'page_groupsmusicians', 100, ['alias' => 'groups'], function ($i) use ($e) {
+                return $e->findChildrenInstances($i, 'group_musicians', 100, ['alias' => 'musicians']);
+            });
+            return array_merge($submenus, $groups);
+        });
+
+        $this->assertEquals(array(), $res);
+
+    }
+
+    public function testOBC2()
+    {
+        $start = microtime(true);
+
+        $id = 25116;
         $params = [
-          'lang' => 'ca'
-          , 'debug' => true
-          , 'metadata' => true
-					, 'show_inmediate_debug' => true
-					, 'timings' => true
-        ];			
-				
-				$e=new Extractor($conn, $params);
-				$res=$e->findInstanceById($id, null, function($i) use ($e)
-				{//page
-					$submenus=$e->findRelatedInstances($i, 'obc_section_pages', 1, ['alias'=>'submenu'], function($i) use ($e){
-						return $e->findRelatedInstances($i, 'obc_section_pages', 100, ['direction'=>'child', 'alias'=>'pages']);
-					});
-					//print_r($submenus);die;
-					$groups=$e->findRelatedInstances($i, 'page_groupsmusicians', 100, ['alias'=>'groups'], function($i) use ($e)
-						{
-						  return $e->findChildrenInstances($i, 'group_musicians', 100, ['alias'=>'musicians']);
-						});
-				  return array_merge($submenus, $groups);	
-				});
-				
-				$end=microtime(true);
-				$total=$end-$start;
-				echo "Tiempo total $total segundos";
-				print_r($res);
-		}
+            'lang' => 'ca'
+            , 'debug' => true
+            , 'metadata' => true
+            , 'show_inmediate_debug' => true
+            , 'timings' => true
+        ];
 
+        $e = new Extractor($this->connection, $params);
+        $res = $e->findInstanceById($id, null, function ($i) use ($e) {//page
+            $submenus = $e->findRelatedInstances($i, 'obc_section_pages', 1, ['alias' => 'submenu'], function ($i) use ($e) {
+                return $e->findRelatedInstances($i, 'obc_section_pages', 100, ['direction' => 'child', 'alias' => 'pages']);
+            });
+            //print_r($submenus);die;
+            $groups = $e->findRelatedInstances($i, 'page_groupsmusicians', 100, ['alias' => 'groups'], function ($i) use ($e) {
+                return $e->findChildrenInstances($i, 'group_musicians', 100, ['alias' => 'musicians']);
+            });
+            return array_merge($submenus, $groups);
+        });
 
-		
+        $end = microtime(true);
+        $total = $end - $start;
+        echo "Tiempo total $total segundos";
+        print_r($res);
+    }
 }
---
+++ tests/Omatech/Editora/TestCaseBase.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by Omatech
+ * User: aroca
+ * Date: 25/04/18 13:44
+ */
+
+
+//declare(strict_types = 1);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+//echo __DIR__."\n";die;
+
+
+$autoload_location = '/vendor/autoload.php';
+while (!is_file(__DIR__.$autoload_location)) { $autoload_location='/..'.$autoload_location;}
+
+$config_location = '/conf/config.php';
+while (!is_file(__DIR__.$config_location)) { $config_location='/..'.$config_location;}
+
+$bootstrap_location = '/conf/bootstrap.php';
+while (!is_file(__DIR__.$bootstrap_location)) { $bootstrap_location='/..'.$bootstrap_location;}
+
+require_once __DIR__.$autoload_location;
+require_once __DIR__.$config_location;
+require_once __DIR__.$bootstrap_location;
+
+use PHPUnit\Framework\TestCase;
+
+class TestCaseBase extends PHPUnit_Framework_TestCase
+{
+    protected $connection;
+
+    public function __construct($name = NULL, array $data = array(), $dataName = '')
+    {
+        global $conn;
+        $this->connection = $conn;
+
+        parent::__construct($name, $data, $dataName);
+    }
+
+    public function testConnection()
+    {
+        //TODO
+        $this->assertTrue(true);
+    }
+
+}
