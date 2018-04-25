c1bef69abf766a7d9807fafdb29d28fb7fbbe312
apons <apons@APONS-HP-EMO.mshome.net>
2018-04-25 13:03:16 +0200

    test commit for packagist

--- tests/Omatech/Editora/Extractor/ExtractorTest.php
+++ tests/Omatech/Editora/Extractor/ExtractorTest.php
@@ -54,6 +54,7 @@ final class ExtractorTest extends PHPUnit_Framework_TestCase {
 				  return array_merge($submenus, $groups);	
 				});
 				
+				
 				$this->assertEquals(array(), $res);
 				
 		}				
